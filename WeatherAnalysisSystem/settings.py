c6c2d9cbc049d0017e583bbc310099d0ef4ac333
muralififa <muralififa1@gmail.com>
2024-09-18 03:44:19 +0530

    Fin

--- WeatherAnalysisSystem/settings.py
+++ WeatherAnalysisSystem/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Ensure this points to your static folder
 ]
 STATICFILES_STORAGE="whitenoise.storage.CompressedMainfestStaticFilesStorage"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Collect static files
 # Ensure `collectstatic` is run after every deployment
 
