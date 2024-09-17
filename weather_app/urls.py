a9f969d0c740bd355514d1234e2cdac1e32762fa
muralififa <muralififa1@gmail.com>
2024-09-18 02:16:08 +0530

    End of processing files

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -3,5 +3,5 @@ from .views import get_weather_data, show_weather_data
 
 urlpatterns = [
     path('', get_weather_data, name='get_weather_data'),
-    path('weather/<str:city>/', show_weather_data, name='show_weather_data'),  # URL to handle city parameter
+    path('weather/<str:city>/', show_weather_data, name='show_weather_data'), 
 ]
